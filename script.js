d39de70b2d4c295906d676cc5ffa39aba4293f7f
kiera <85643272+mintykiera@users.noreply.github.com>
2023-01-15 01:15:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,38 @@
 document.getElementById('submit-btn').addEventListener('click', function () {
-  // Collect user input and convert to uppercase
-  var fil = document.getElementById('fil').value.toUpperCase();
-  var eng = document.getElementById('eng').value.toUpperCase();
-  var math = document.getElementById('math').value.toUpperCase();
-  var sci = document.getElementById('sci').value.toUpperCase();
-  var ap = document.getElementById('ap').value.toUpperCase();
-  var clve = document.getElementById('clve').value.toUpperCase();
-  var tle = document.getElementById('tle').value.toUpperCase();
-  var mapeh = document.getElementById('mapeh').value.toUpperCase();
-  var comp = document.getElementById('comp').value.toUpperCase();
+  var validInput = /^(A|P|B|E|AP|IE)$/;
+  var grades = [
+    'fil',
+    'eng',
+    'math',
+    'sci',
+    'ap',
+    'clve',
+    'tle',
+    'mapeh',
+    'comp',
+  ];
+  var numGrades = [];
+  var missing = false;
 
-  // validate user input
-  if (!/^(A|P|B|E|AP|IE)$/.test(fil)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(eng)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(math)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(sci)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(ap)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(clve)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(tle)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(mapeh)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
-  }
-  if (!/^(A|P|B|E|AP|IE)$/.test(comp)) {
-    alert('Invalid input, please use a valid letter grade.');
-    return;
+  for (var i = 0; i < grades.length; i++) {
+    var grade = document.getElementById(grades[i]).value.toUpperCase();
+
+    if (!validInput.test(grade)) {
+      alert('Invalid input, please use a valid letter grade.');
+      return;
+    } else if (grade == '') {
+      missing = true;
+    } else {
+      numGrades.push(grade);
+    }
   }
 
-  // Check for missing grades
-  if (
-    fil == '' ||
-    eng == '' ||
-    math == '' ||
-    sci == '' ||
-    ap == '' ||
-    clve == '' ||
-    tle == '' ||
-    mapeh == '' ||
-    comp == ''
-  ) {
+  if (missing) {
     alert('Please fill out all subjects.');
     return;
   }
 
-  // Map letter grades to set variables
+  //Map letter grades to set variables
   var A = Math.floor(Math.random() * (100 - 95 + 1)) + 95;
   var P = Math.floor(Math.random() * (94 - 90 + 1)) + 90;
   var AP = Math.floor(Math.random() * (89 - 85 + 1)) + 85;
@@ -73,125 +41,22 @@ document.getElementById('submit-btn').addEventListener('click', function () {
   var IE = Math.floor(Math.random() * (74 - 70 + 1)) + 70;
 
   // Collect number values for letter grades
-  var numGrades = [];
-  if (fil == 'A') {
-    numGrades.push(A);
-  } else if (fil == 'P') {
-    numGrades.push(P);
-  } else if (fil == 'AP') {
-    numGrades.push(AP);
-  } else if (fil == 'E') {
-    numGrades.push(E);
-  } else if (fil == 'B') {
-    numGrades.push(B);
-  } else if (fil == 'IE') {
-    numGrades.push(IE);
-  }
-  if (eng == 'A') {
-    numGrades.push(A);
-  } else if (eng == 'P') {
-    numGrades.push(P);
-  } else if (eng == 'AP') {
-    numGrades.push(AP);
-  } else if (eng == 'E') {
-    numGrades.push(E);
-  } else if (eng == 'B') {
-    numGrades.push(B);
-  } else if (eng == 'IE') {
-    numGrades.push(IE);
-  }
-  if (math == 'A') {
-    numGrades.push(A);
-  } else if (math == 'P') {
-    numGrades.push(P);
-  } else if (math == 'AP') {
-    numGrades.push(AP);
-  } else if (math == 'E') {
-    numGrades.push(E);
-  } else if (math == 'B') {
-    numGrades.push(B);
-  } else if (math == 'IE') {
-    numGrades.push(IE);
-  }
-  if (sci == 'A') {
-    numGrades.push(A);
-  } else if (sci == 'P') {
-    numGrades.push(P);
-  } else if (sci == 'AP') {
-    numGrades.push(AP);
-  } else if (sci == 'E') {
-    numGrades.push(E);
-  } else if (sci == 'B') {
-    numGrades.push(B);
-  } else if (sci == 'IE') {
-    numGrades.push(IE);
+  for (var i = 0; i < numGrades.length; i++) {
+    if (numGrades[i] == 'A') {
+      numGrades[i] = A;
+    } else if (numGrades[i] == 'P') {
+      numGrades[i] = P;
+    } else if (numGrades[i] == 'AP') {
+      numGrades[i] = AP;
+    } else if (numGrades[i] == 'E') {
+      numGrades[i] = E;
+    } else if (numGrades[i] == 'B') {
+      numGrades[i] = B;
+    } else if (numGrades[i] == 'IE') {
+      numGrades[i] = IE;
+    }
   }
 
-  if (ap == 'A') {
-    numGrades.push(A);
-  } else if (ap == 'P') {
-    numGrades.push(P);
-  } else if (ap == 'AP') {
-    numGrades.push(AP);
-  } else if (ap == 'E') {
-    numGrades.push(E);
-  } else if (ap == 'B') {
-    numGrades.push(B);
-  } else if (ap == 'IE') {
-    numGrades.push(IE);
-  }
-  if (clve == 'A') {
-    numGrades.push(A);
-  } else if (clve == 'P') {
-    numGrades.push(P);
-  } else if (clve == 'AP') {
-    numGrades.push(AP);
-  } else if (clve == 'E') {
-    numGrades.push(E);
-  } else if (clve == 'B') {
-    numGrades.push(B);
-  } else if (clve == 'IE') {
-    numGrades.push(IE);
-  }
-  if (tle == 'A') {
-    numGrades.push(A);
-  } else if (tle == 'P') {
-    numGrades.push(P);
-  } else if (tle == 'AP') {
-    numGrades.push(AP);
-  } else if (tle == 'E') {
-    numGrades.push(E);
-  } else if (tle == 'B') {
-    numGrades.push(B);
-  } else if (tle == 'IE') {
-    numGrades.push(IE);
-  }
-  if (mapeh == 'A') {
-    numGrades.push(A);
-  } else if (mapeh == 'P') {
-    numGrades.push(P);
-  } else if (mapeh == 'AP') {
-    numGrades.push(AP);
-  } else if (mapeh == 'E') {
-    numGrades.push(E);
-  } else if (mapeh == 'B') {
-    numGrades.push(B);
-  } else if (mapeh == 'IE') {
-    numGrades.push(IE);
-  }
-  if (comp == 'A') {
-    numGrades.push(A);
-  } else if (comp == 'P') {
-    numGrades.push(P);
-  } else if (comp == 'AP') {
-    numGrades.push(AP);
-  } else if (comp == 'E') {
-    numGrades.push(E);
-  } else if (comp == 'B') {
-    numGrades.push(B);
-  } else if (comp == 'IE') {
-    numGrades.push(IE);
-  }
   // Find general average
   var sum = 0;
   for (var i = 0; i < numGrades.length; i++) {
